9ff43f1c20f3d6d9d97dd71cb688a14e3358139f
Dawid Mejka <dawmejka@gmail.com>
2020-01-29 13:08:04 +0100

    Make home arrow work

---
+++ js/home_arrow.js
@@ -0,0 +1,23 @@
+const arrow = document.querySelector("footer .arrow-up");
+let active = false;
+
+function showArrow() {
+    console.log(window.scrollY);
+    if (window.scrollY > 500 && !active) {
+        active = !active;
+        arrow.classList.add('active')
+    } else if (window.scrollY == 0 && active) {
+        active = !active;
+        arrow.classList.remove('active');
+    }
+}
+
+function returnHome() {
+    document.documentElement.scrollTop -= 20;
+    if (document.documentElement.scrollTop > 0) {
+        setTimeout(returnHome, 5);
+    } else document.documentElement.scrollTop = 0;
+}
+
+arrow.addEventListener('click', returnHome);
+window.addEventListener('scroll', showArrow);
