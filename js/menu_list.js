6f275a197cccac6ddfb069940f9b77368b6a6fed
Dawid Mejka <dawmejka@gmail.com>
2020-01-29 12:33:11 +0100

    complete menu page

--- js/menu_list.js
+++ js/menu_list.js
@@ -1,13 +1,26 @@
 const btnMenu = document.querySelector('.menu-main button');
+const btnArrow = document.querySelector('.menu-main button i');
 const menuOptions = document.querySelector('.menu-options');
 const optionsList = document.querySelectorAll('.menu-options li');
+const menuMeals = document.querySelectorAll('.menu-main .meals ul');
 
 function showOptions() {
     menuOptions.classList.toggle('active');
+    btnArrow.classList.toggle('fa-chevron-down');
+    btnArrow.classList.toggle('fa-chevron-up');
 }
 
 function changeOption() {
     btnMenu.querySelector('span').textContent = this.textContent;
+    menuMeals.forEach(list => {
+        if (list.classList.contains('active')) {
+            list.classList.remove('active')
+        }
+        if (list.classList.contains(this.textContent.toLowerCase())) {
+            list.classList.add('active');
+        }
+    })
+    showOptions();
     // console.log(this);
 }
 
